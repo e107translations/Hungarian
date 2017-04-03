4d24aeb9b9c4a6ad497a250c7af68bb7eace388a
Gábor <yesszus@gmail.com>
2017-04-03 21:42:21 +0200

    Add files via upload

--- e107_plugins/import/languages/Hungarian_admin.php
+++ e107_plugins/import/languages/Hungarian_admin.php
@@ -1,13 +1,21 @@
 <?php
+/*
++--------------------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/04/01 18:58:23
+|
+|        $Author: Yesszus $
+|        Web: https://github.com/yesszus/e107-v2.x-Language-Packs/releases
+|
++--------------------------------------------------------------------------+
+*/
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
-
-define("LAN_CONVERT_03", "Fontos, hogy frissen telepített, üres e107 adatbázist használj, amiben egyedül a fő-adminisztrátor található (ID=1)");
-define("LAN_CONVERT_04", "Kitöltendő mezők maradtak üresen, kérlek lépj vissza, és add meg értékeiket.");
-define("LAN_CONVERT_05", "Ezt a scriptet lefuttatva előfordulhat, hogy egyes adatbázis táblák kiürítésre kerülnek. Kérlek győződj meg róla, hogy rendelkezel teljes adatbázis-mentéssel!");
-define("LAN_CONVERT_06", "Importálandó adattípus");
+define("LAN_CONVERT_03", "Fontos, hogy frissen telepített, üres e107 adatbázist használj, amiben egyedül a főadminisztrátor található (ID=1)");
+define("LAN_CONVERT_04", "Kitöltendő mezők maradtak üresen! Kérlek lépj vissza, és add meg értékeiket.");
+define("LAN_CONVERT_05", "Ezt a scriptet lefuttatva előfordulhat, hogy egyes adatbázis táblák kiürítésre kerülnek. Kérlek győződj meg róla, hogy rendelkezel-e teljes adatbázis-mentéssel!");
+define("LAN_CONVERT_06", "Importálandó adattipus");
 define("LAN_CONVERT_07", "CSV formátum specifikációja");
 define("LAN_CONVERT_08", "Létező adatbázis");
 define("LAN_CONVERT_09", "A (forrás) adatbázishoz való kapcsolódáshoz szükséges adatok megadása");
@@ -30,18 +38,18 @@ define("LAN_CONVERT_25", "Felhasználók");
 define("LAN_CONVERT_26", "Fórum részek");
 define("LAN_CONVERT_27", "Szavazások");
 define("LAN_CONVERT_28", "Hírek");
-define("LAN_CONVERT_29", "Adatbázis importálása befejeződött");
+define("LAN_CONVERT_29", "Az adatbázis importálása befejeződött");
 define("LAN_CONVERT_30", "Importálás rutin információi");
-define("LAN_CONVERT_31", "CSV adatfájl nem létezik, vagy nincs jogosultság az olvasásához");
+define("LAN_CONVERT_31", "A CSV adatfájl nem létezik, vagy nincs jogosultság az olvasásához");
 define("LAN_CONVERT_32", "Hiba a CSV adatfájl olvasása közben");
 define("LAN_CONVERT_33", "Hiba a CSV adatfájl egy sorában ");
 define("LAN_CONVERT_34", "Hiba: --ERRNUM-- felhasználói adatbázis írása közben ");
-define("LAN_CONVERT_35", "CSV importálása befejeződött. --LINES-- sor kiolvasva, --USERS-- felhasználó létrehozva, --ERRORS-- hiba");
-define("LAN_CONVERT_36", "CSV adat fájlneve");
-define("LAN_CONVERT_37", "Érvénytelen formátumú specifikáció az importlandó típushoz");
+define("LAN_CONVERT_35", "A CSV importálás befejeződött. --LINES-- sor kiolvasva, --USERS-- felhasználó létrehozva, --ERRORS-- hiba");
+define("LAN_CONVERT_36", "A CSV adat fájlneve");
+define("LAN_CONVERT_37", "Érvénytelen formátumú specifikáció az importlandó tipushoz");
 define("LAN_CONVERT_38", "Meglévő adatok törlése");
 define("LAN_CONVERT_39", "(Ha nem, akkor az importált adat \"Anonymous\"-ként lesz látható)");
-define("LAN_CONVERT_40", "Meglévő adatok törölve");
+define("LAN_CONVERT_40", "A meglévő adatok törölve");
 define("LAN_CONVERT_41", "Szükséges adatbázis (jogosultság) mező maradt üresen");
 define("LAN_CONVERT_42", "Hiba a definíciós fájlban - a szükséges csoport nem létezik");
 define("LAN_CONVERT_43", "Hiba a (forrás) adatbázishoz való kapcsolódás közben");
@@ -50,7 +58,6 @@ define("LAN_CONVERT_45", "Nem lehet olvasni az importálandó kód-fájlt");
 define("LAN_CONVERT_46", "Hiba: --ERRNUM-- a(z) --DB-- adatbázisírása közben, sor: ");
 define("LAN_CONVERT_47", "--blokk-- kötegelt importálás befejeződött. --LINES-- sor kiolvasva, --USERS-- felhasználó létrehozva, --ERRORS-- hiba");
 define("LAN_CONVERT_48", "Fórum bejegyzések");
-
 define("LAN_CONVERT_49", "Drupal");
 define("LAN_CONVERT_50", "Alap adatok importálása");
 define("LAN_CONVERT_51", "Az importáláshoz felhasznált Drupal verziója.");
@@ -59,3 +66,22 @@ define("LAN_CONVERT_53", "Drupal oldal URL címe (pl. http://mydrupalsite.com)."
 define("LAN_CONVERT_54", "Drupal URL címe");
 define("LAN_CONVERT_55", "Alap útvonal, mely a webcím után jelenik meg (pl. /drupal /).");
 define("LAN_CONVERT_56", "Drupal útvonala");
+define("LAN_CONVERT_57", "Nincs hiba!");
+define("LAN_CONVERT_58", "Nem lehet megváltoztatni a főadmin adatokat!");
+define("LAN_CONVERT_59", "érvénytelen mező!");
+define("LAN_CONVERT_60", "A kötelező mező nincs megadva!");
+define("LAN_CONVERT_61", "A felhasználó már létezik!");
+define("LAN_CONVERT_62", "Érvénytelen karakterek a felhasználó vagy a bejelentkező névben!");
+define("LAN_CONVERT_63", "Hiba történt a kiterjesztett felhasználói mezők mentése közben!");
+define("LAN_CONVERT_64", "Kiválaszt");
+define("LAN_CONVERT_65", "Oldalak");
+define("LAN_CONVERT_66", "Oldal fejezetek");
+define("LAN_CONVERT_67", "Linkek");
+define("LAN_CONVERT_68", "Média");
+define("LAN_CONVERT_69", "Fórum");
+define("LAN_CONVERT_70", "Fórum topikok/témák");
+define("LAN_CONVERT_71", "Fórum bejegyzések");
+define("LAN_CONVERT_72", "Fórum követés");
+define("LAN_CONVERT_73", "Felhasználói csoportok");
+define("LAN_CONVERT_74", "Hír kategóriák");
+?>

2ec2a289435d658868a2a9619090bbd5755e6941
Gábor <yesszus@gmail.com>
2017-04-22 14:19:30 +0200

    Add files via upload

--- e107_plugins/social/languages/Hungarian/Hungarian_front.php
+++ e107_plugins/social/languages/Hungarian/Hungarian_front.php
@@ -1,14 +1,17 @@
 <?php
 /*
-+---------------------------------------------------------------+
++---------------------------------------------------------------------------+
 |        e107 website content management system Hungarian Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2016/05/22 14:04:14
+|        Last Modified: 2017/04/22 13:43:21
 |
 |        $Author: Yesszus $
-+---------------------------------------------------------------+
+|        Web: https://github.com/yesszus/e107-v2.x-Language-Packs/releases
+|
++---------------------------------------------------------------------------+
 */
+
 define("LAN_SOCIAL_000", "Megosztás itt: [x]");
 define("LAN_SOCIAL_001", "Lájkolás itt: [x]");
 define("LAN_SOCIAL_002", "Email küldése");
@@ -23,5 +26,7 @@ define("LAN_SOCIAL_203",  "Tweet üzenet írása.");
 define("LAN_SOCIAL_204",  "Megosztás");
 define("LAN_SOCIAL_205",  "A hozzászólásokat nem lehet betölteni! Hiányzik a Facebook appID.");
 define("LAN_SOCIAL_WARNING", "A Facebook hozzászólásokhoz szükséges egy facebook App ID.<br>Lépjen a 'Regisztráció/bejelentkezés' területre az admin felület=> beállításoknál.");
+define("LAN_SOCIAL_206",  "Közösségi Bejelentkezés Tesztelés");
+define("LAN_SOCIAL_207",  "Mielőtt tesztelné az e107 új felhasználó bejelentkezés/regisztráció funkciót, kérjük jelentkezzen ki.");
 
 ?>

7f6536369dcded423a57a140b9d94c49a5b5db2b
Gábor <yesszus@gmail.com>
2017-06-05 10:02:58 +0200

    Add files via upload

--- e107_plugins/featurebox/languages/Hungarian_admin_featurebox.php
+++ e107_plugins/featurebox/languages/Hungarian_admin_featurebox.php
@@ -1,13 +1,31 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
+/*
++--------------------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/06/05 08:51:43
+|
+|        $Author: Yesszus $
+|        Web: https://github.com/yesszus/e107-v2.x-Language-Packs/releases
+|
++--------------------------------------------------------------------------+
+*/
 
 define("FBLAN_08", "Üzenet szövege");
 define("FBLAN_12", "Mód");
 define("FBLAN_13", "Üzenetek véletlenszerű forgatása");
-define("FBLAN_14", "Mutasd csak ezt az üzenetet");
+define("FBLAN_14", "Csak ezt az üzenetet mutasd");
 define("FBLAN_22", "Renderelés típusa");
 define("FBLAN_23", "Dobozban");
 define("FBLAN_24", "Egyszerű");
+define("FBLAN_25", "Kategória létrehozása");
+define("FBLAN_26", "Kép/Videó"); 
+define("FBLAN_27", "Kép Link"); 
+define("FBLAN_28", "Featurebox Menü Kategória"); 
+define("FBLAN_29", "A Featurebox menü által használt kategória"); 
+define("FBLAN_30", "Kategória sablon"); 
+define("FBLAN_31", "Véletlenszerű"); 
+define("FBLAN_32", "Paraméterek (opcionális)"); 
+define("FBLAN_33", "Opcionális Javascript Paraméterek (a formátum változtatható)"); 
+?>
--- e107_plugins/featurebox/languages/Hungarian_global.php
+++ e107_plugins/featurebox/languages/Hungarian_global.php
@@ -1,8 +1,16 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
+/*
++--------------------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/30 08:51:43
+|
+|        $Author: Yesszus $
+|        Web: https://github.com/yesszus/e107-v2.x-Language-Packs/releases
+|
++--------------------------------------------------------------------------+
+*/
 
 define("LAN_PLUGIN_FEATUREBOX_NAME", "Feature Box");
 define("LAN_PLUGIN_FEATUREBOX_DESCRIPTION", "Hírek és egyéb elemek megjelenítése az oldal tetején egy animált területen.");
@@ -13,3 +21,4 @@ define("FBLAN_INSTALL_02", "Alapértelmezett értékek hozzáadása.");
 define("FBLAN_INSTALL_03", "Nincs hozzárendelve");
 define("FBLAN_INSTALL_04", "Carousel");
 define("FBLAN_INSTALL_05", "Fülek");
+?>

297fa6bb34634b32e7dbeee7d8ff55a7220cc3b7
Gábor <yesszus@gmail.com>
2017-06-15 08:38:20 +0200

    Add files via upload

--- e107_plugins/poll/languages/Hungarian_admin_poll.php
+++ e107_plugins/poll/languages/Hungarian_admin_poll.php
@@ -1,20 +1,31 @@
 <?php
+/*
++--------------------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/06/15 08:33:43
+|
+|        $Author: Yesszus $
+|        Web: https://github.com/yesszus/e107-v2.x-Language-Packs/releases
+|
++--------------------------------------------------------------------------+
+*/
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
-
-define("POLL_ADLAN03", "Szavazás beállítása");
+//define("POLL_ADLAN03", "Szavazás beállítása");
 define("POLL_ADLAN04", "A szavazás plugin telepítve. Szavazás létrehozásához kattints a Szavazás ikonra az admin területen, és aktiváld a menüt a Menü-kezelőben.");
-define("POLL_ADLAN05", "Fő szavazás: ");
-define("POLL_ADLAN06", "Fórum téma: ");
-define("POLL_ADLAN07", "Típus ");
+//define("POLL_ADLAN05", "Fő szavazás: ");
+//define("POLL_ADLAN06", "Fórum téma: ");
+//define("POLL_ADLAN07", "Típus ");
 
-define("POLLAN_MENU_CAPTION", "Szavazás");
+//define("POLLAN_MENU_CAPTION", "Szavazás");
 
-define("POLLAN_7", "Még nincs szavazás.");
-define("LAN_AL_POLL_01", "Szavazás törölve");
-define("LAN_AL_POLL_02", "Szavazás frissítve");
-define("LAN_AL_POLL_03", "Szavazás létrehozva");
-define("LAN_AL_POLL_04", "");
-define("LAN_AL_POLL_05", "");
+//define("POLLAN_7", "Még nincs szavazás.");
+
+//define("LAN_AL_POLL_01", "Szavazás törölve");
+//define("LAN_AL_POLL_02", "Szavazás frissítve");
+//define("LAN_AL_POLL_03", "Szavazás létrehozva");
+//define("LAN_AL_POLL_04", "");
+//define("LAN_AL_POLL_05", "");
+
+?>
--- e107_plugins/poll/languages/Hungarian_global.php
+++ e107_plugins/poll/languages/Hungarian_global.php
@@ -1,8 +1,18 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
+/*
++--------------------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/06/15 08:33:43
+|
+|        $Author: Yesszus $
+|        Web: https://github.com/yesszus/e107-v2.x-Language-Packs/releases
+|
++--------------------------------------------------------------------------+
+*/
 
 define("LAN_PLUGIN_POLL_NAME", "Szavazás");
 define("LAN_PLUGIN_POLL_DESCRIPTION", "Szavazások létrehozására használatos plugin.");
+
+?>

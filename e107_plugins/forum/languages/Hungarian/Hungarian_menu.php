067ff27f0686ef530bc76d38f28d97309290ade9
Gábor <yesszus@gmail.com>
2017-09-25 11:42:14 +0200

    Update Hungarian_menu.php

--- e107_plugins/forum/languages/Hungarian/Hungarian_menu.php
+++ e107_plugins/forum/languages/Hungarian/Hungarian_menu.php
@@ -1,19 +1,30 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
-
+/*
++--------------------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/09/25 11:36:43
+|
+|        $Author: Yesszus $
+|        Web: https://github.com/yesszus/e107-v2.x-Language-Packs/releases
+|
++--------------------------------------------------------------------------+
+*/
 define("LAN_FORUM_MENU_001", "Írta:");
 define("LAN_FORUM_MENU_002", "Még nincs bejegyzés");
-define("LAN_FORUM_MENU_003", "Új fórum üzenetek menü beállítása sikerült");
+define("LAN_FORUM_MENU_003", "Sikeres új fórum üzenetek menü beállítás!");
 define("LAN_FORUM_MENU_004", "Felirat");
 define("LAN_FORUM_MENU_005", "Hány bejegyzés legyen megjelenítve?");
 define("LAN_FORUM_MENU_006", "Hány karakter legyen megjelenítve?");
 define("LAN_FORUM_MENU_007", "Utótag a hosszú bejegyzések levágása után?");
 define("LAN_FORUM_MENU_008", "Téma megjelenítése a menüben?");
-define("LAN_FORUM_MENU_009", "Menü-beállítások frissítése");
-define("LAN_FORUM_MENU_0010", "Új fórum üzenetek menü beállításai");
+define("LAN_FORUM_MENU_009", "Menü beállítások frissítése");
+//define("LAN_FORUM_MENU_0010", "Új fórum üzenetek menü beállítások");
 
-define("LAN_FORUM_MENU_0012", "Maximum ennyi időre visszamenőleg jelenjenek meg bejegyzések");
-define("LAN_FORUM_MENU_0013", "Az értéket napokban megadva csökkentheti a lekérdezéshez szükséges időt nagyobb terhelés esetén.");
+define("LAN_FORUM_MENU_012", "Maximum ennyi időre visszamenőleg jelenjenek meg bejegyzések");
+define("LAN_FORUM_MENU_013", "Az értéket napokban megadva csökkentheti a lekérdezéshez szükséges időt nagyobb terhelés esetén.");
+define("LAN_FORUM_MENU_014", "Görgetési magasság (pixelben)");
+define("LAN_FORUM_MENU_015", "Hagyd üresen, ha nincs görgetés");
+
+?>

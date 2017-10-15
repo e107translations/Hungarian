9698ee95c7ed4b0ed518a85cfd2191cc919e4618
Gábor <yesszus@gmail.com>
2017-10-15 10:38:26 +0200

    Update Hungarian_admin.php

--- e107_plugins/newsfeed/languages/Hungarian_admin.php
+++ e107_plugins/newsfeed/languages/Hungarian_admin.php
@@ -1,20 +1,20 @@
 <?php
 /*
-+---------------------------------------------------------------+
++-----------------------------------------------------------------------+
 |        e107 website content management system Hungarian Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2016/03/19 14:11:14
+|        Last Modified: 2017/10/15 10:28:14
 |
 |        $Author: Yesszus $
-+---------------------------------------------------------------+
++-----------------------------------------------------------------------+
 */
 
 define("NFLAN_03", "Hírforrások beállítása");
-define("NFLAN_04", "A Hírforrások plugin telepítve. Hírforrás felvételéhez és a beállításokhoz, lépj vissza az admin főoldalra és kattints a Hírforrások ikonra a plugin manager-ben.");
+define("NFLAN_04", "A Hírforrások plugin telepítve! Hírforrás felvételéhez és a beállításokhoz, lépj vissza az admin főoldalra és kattints a Hírforrások ikonra a plugin manager-ben.");
 define("NFLAN_07", "Jelenlegi hírforrások");
 define("NFLAN_08", "Hírforrások főoldala");
-define("NFLAN_09", "Hírforrás felvitele");
+define("NFLAN_09", "Hírforrás felvétele");
 define("NFLAN_10", "RSS hír URL cím.");
 define("NFLAN_11", "Kép elérési útvonala");
 define("NFLAN_12", "Aktiválás");
@@ -29,35 +29,33 @@ define("NFLAN_22", "válaszd ki, hol jelenjen meg a hírforrás");
 define("NFLAN_26", "Frissítési gyakoriság");
 define("NFLAN_37", "Hírforrások leírásának sorrendje, írd be a 'default' értéket az alapértelmezett leírás használatához a hírforrásokban");
 define("NFLAN_41", "Nincs hírforrás megadva");
-define("NFLAN_42", "<b>»</b> [h=4]Hírforrás neve:[/h]
+define("NFLAN_42", "[b]»[/b] [h=4]Hírforrás neve:[/h]
   A hírforrás azonosítóneve, írj be bármit, ami tetszik.
-  <br /><br />
-  <b>»</b> [h=4]Az rss hírforrás URL-je:[/h]
+  [br][br]
+  [b]»[/b] [h=4]Az rss hírforrás URL-je:[/h]
   Az rss hírforrás címe.
-  <br /><br />
-  <b>»</b> [h=4]Kép útvonala (URL):[/h]
-  Ha a hírforrás rendelkezik képpel, írd be a 'default' értéket a használatához. Saját kép használata esetén írd be a kép teljes elérési útvonalát. Hagyd üresen, ha ne jelenjen meg kép.
-  <br /><br />
-  <b>»</b> [h=4]Ismertetés:[/h]
+  [br][br]
+  [b]»[/b] [h=4]Kép útvonala (URL):[/h]
+  Ha a hírforrás rendelkezik képpel, írd be a 'default' értéket a használatához.[br]Saját kép használata esetén írd be a kép teljes elérési útvonalát.[br]Hagyd üresen, ha ne jelenjen meg kép.
+  [br][br]
+  [b]»[/b] [h=4]Ismertetés:[/h]
   Írj egy rövid ismertetést a hírforráshoz vagy hagyd 'default' értéken a hírforrásban megadott ismertetés megjelenítéséhez (ha van benne).
-  <br /><br />
-  <b>»</b> [h=4]Frissítési intervallum másodpercben:[/h]
+  [br][br]
+  [b]»[/b] [h=4]Frissítési intervallum másodpercben:[/h]
   A másodpercek mennyisége, mielőtt a hírforrás frissíti magát, pl.: 1800: 30 perc, 3600: egy óra.
-  <br /><br />
-  <b>»</b> [h=4]Aktiválás:[/h]
+  [br][br]
+  [b]»[/b] [h=4]Aktiválás:[/h]
   Hol akarod, hogy megjelenjen a hírforrás eredménye, a menü-szerű hírforrás megjelenítéséhez aktiválni kell a <a href='".e_ADMIN."menus.php'>[b]menük oldalon[/b]</a>.
-  <br /><br />[h=4]Tipp[/h]Egy hasznos, felhasználható hírforrás lista megtekinthető a [link=https://www.dmoz.org/Computers/Internet/On_the_Web/Syndication_and_Feeds/RSS/Directories/ external]dmoz[/link] vagy [link=http://www.feedster.com/ external]feedster.com[/link] oldalon.");
+  [br][br][h=4]Tipp[/h]Egy hasznos, felhasználható hírforrás lista megtekinthető a [link=https://www.dmoz.org/Computers/Internet/On_the_Web/Syndication_and_Feeds/RSS/Directories/ external]dmoz[/link] vagy [link=http://www.feedster.com/ external]feedster.com[/link] oldalon.");
 define("NFLAN_43", "Hírforrás - Súgó");
 define("NFLAN_44", "katt ide a megtekintéshez");
 define("NFLAN_45", "Menüben megjelenő hírforrások száma");
 define("NFLAN_46", "A főoldalon megjelenő hírforrások száma");
 define("NFLAN_47", "0 vagy hagyd üresen az összes megjelenítéséhez");
-define("NFLAN_49", "Nem lehet deszerializálni az rss adatot - nem szabványos szintaxist használ");
+define("NFLAN_49", "Nem lehet beolvasni az rss adatokat, vagy nem szabványos szintaxist használ");
 define("LAN_AL_NEWSFD_01", "Hírforrás létrehozva");
 define("LAN_AL_NEWSFD_02", "Hírforrás frissítve");
 define("LAN_AL_NEWSFD_03", "Hírforrás törölve");
-define("LAN_AL_NEWSFD_04", "");
-define("LAN_AL_NEWSFD_05", "");
 define("NFLAN_50", "Utolsó Frissítés");
 
 ?>

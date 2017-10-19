28dcb314d8dc9bddfa8ae3e75ff7ae5e3db466b3
Gábor <yesszus@gmail.com>
2017-10-19 05:24:07 +0200

    Update Hungarian_front.php

--- e107_plugins/faqs/languages/Hungarian/Hungarian_front.php
+++ e107_plugins/faqs/languages/Hungarian/Hungarian_front.php
@@ -4,27 +4,26 @@
 |        e107 website content management system Hungarian Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2017/04/10 07:56:44
+|        Last Modified: 2017/10/19 05:22:44
 |
 |        $Author: Yesszus $
 +----------------------------------------------------------------------+
 */
 
-define("LAN_FAQS_TAGS", "Címkék");
-define("LAN_FAQS_FILTER_ACTIVE", "Aktív");
-define("LAN_FAQS_NONE_AVAILABLE", "Jelenleg nincs GYIK bejegyzés.");
-define("LAN_FAQS_ASKQUESTION_AFTER", "Köszönjük! A kérdésed el lett mentve és remélhetőleg hamarosan meg is lesz válaszolva..");
-define("LAN_FAQS_LIMIT_REACHED", "Elérte a feltehető új kérdések maximális számát! Egyszerre több meglévő kérdésre is kérhet választ.");
-define("LAN_FAQS_REMOVE", "Törlés");
-define("LAN_FAQS_RESET", "Visszaállítás");
-define("LAN_FAQS_X_NOT_FOUND", "[x]: Nem található a keresési eredmények között!");
-define("LAN_FAQS_MODERATE_COMMENTS", "hozzászólások moderálása");
-define("LAN_FAQS_BACK_TO_CATEGORIES", "Vissza a kategóriákhoz");
-define("LAN_FAQS_FAQ", "GYIK");
-
-define("LAN_FAQS_ENTER_A_QUESTION_HERE", "Adja meg a kérdés szövegét...");
-define("LAN_FAQS_CHAR_LIMIT", "karaktert használhat");
-define("LAN_FAQS_PLEASE_REGISTER", "A kérdés beküldéséhez, kérjük [regisztráljon] és/vagy jelentkezzen be.");
-define("LAN_FAQS_NOT_PERMITTED", "Nem engedélyezett.");
-define("LAN_FAQS_YOUR_REQUESTED_FAQS", "Azt kérte, hogy válaszoljanak önnek a következő kérdésekre:");
+define("LAN_FAQS_001", "Címkék");
+define("LAN_FAQS_002", "Aktív");
+define("LAN_FAQS_003", "Jelenleg nincs GYIK bejegyzés.");
+define("LAN_FAQS_004", "Köszönjük! A kérdésed el lett mentve és remélhetőleg hamarosan meg is lesz válaszolva.");
+define("LAN_FAQS_005", "Elérte a feltehető új kérdések maximális számát! Egyszerre több meglévő kérdésre is kérhet választ.");
+define("LAN_FAQS_006", "Törlés");
+define("LAN_FAQS_007", "Visszaállítás");
+define("LAN_FAQS_008", "[x]: Nem található a keresési eredmények között!");
+define("LAN_FAQS_009", "hozzászólások moderálása");
+define("LAN_FAQS_010", "Vissza a kategóriákhoz");
+define("LAN_FAQS_011", "GYIK");
+define("LAN_FAQS_012", "Adja meg a kérdés szövegét...");
+define("LAN_FAQS_013", "karaktert használhat");
+define("LAN_FAQS_014", "A kérdés beküldéséhez, kérjük [regisztráljon] és/vagy jelentkezzen be.");
+define("LAN_FAQS_015", "Nem engedélyezett.");
+define("LAN_FAQS_016", "Azt kérte, hogy válaszoljanak önnek a következő kérdésekre:");
 ?>

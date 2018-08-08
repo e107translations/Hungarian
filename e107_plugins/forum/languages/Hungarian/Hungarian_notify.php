28d3749bfcaa313ae58af5364b74bf3aa210c7b8
Gábor <yesszus@gmail.com>
2018-08-08 08:31:17 +0200

    Update Hungarian_notify.php

--- e107_plugins/forum/languages/Hungarian/Hungarian_notify.php
+++ e107_plugins/forum/languages/Hungarian/Hungarian_notify.php
@@ -1,18 +1,48 @@
 <?php
+/*
++--------------------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2018/08/08 7:34:43
+|
+|        $Author: Yesszus $
+|        Web: https://github.com/e107translations/Hungarian/releases
+|
++--------------------------------------------------------------------------+
+*/
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
-
-define("LAN_FORUM_NT_7", "Fórum - téma létrehozva új felhasználó által");
+define("LAN_FORUM_NT_7", "Fórum - téma létrehozva egy új felhasználó által");
 define("LAN_FORUM_NT_8", "Fórum - téma törölve lett");
 define("LAN_FORUM_NT_9", "Fórum - téma darabolva lett");
 define("LAN_FORUM_NT_10", "Fórum - bejegyzés törölve lett");
 define("LAN_FORUM_NT_11", "Fórum - bejegyzés jelentve lett");
+define("LAN_FORUM_NT_12", "Fórum - téma frissítve");
+define("LAN_FORUM_NT_13", "Fórum - téma áthelyezve");
+define("LAN_FORUM_NT_14", "Fórum - bejegyzés létrehozva");
+define("LAN_FORUM_NT_15", "Fórum - bejegyzés frissítve");
 
 define("LAN_FORUM_NT_NEWTOPIC", "Új téma lett létrehozva");
-define("LAN_FORUM_NT_NEWTOPIC_PROB", "Új téma lett létrehozva \"próbaidős\" felhasználó által");
-define("LAN_FORUM_NT_TOPIC_DELETED", "Téma törölve lett");
-define("LAN_FORUM_NT_TOPIC_SPLIT", "Téma darabolva lett");
-define("LAN_FORUM_NT_POST_DELETED", "Bejegyzés törölve lett");
-define("LAN_FORUM_NT_POST_REPORTED", "Bejegyzés jelentve lett");
+define("LAN_FORUM_NT_NEWTOPIC_PROB", "Új téma lett létrehozva egy próbaidős felhasználó által");
+define("LAN_FORUM_NT_TOPIC_DELETED", "A téma törölve lett");
+define("LAN_FORUM_NT_TOPIC_SPLIT", "A téma darabolva lett");
+define("LAN_FORUM_NT_POST_DELETED", "A bejegyzés törölve lett");
+define("LAN_FORUM_NT_POST_REPORTED", "A bejegyzés jelentve lett");
+define("LAN_FORUM_NT_TOPIC_UPDATED", "A téma frissítve");
+define("LAN_FORUM_NT_TOPIC_MOVED", "A téma áthelyezve");
+define("LAN_FORUM_NT_POST_CREATED", "A bejegyzés létrehozva");
+define("LAN_FORUM_NT_POST_UPDATED", "A bejegyzés frissítve");
+
+
+
+define("LAN_FORUM_NT_NEWTOPIC_MSG", "[user] egy új témát hozott létre a(z) [forum] fórumban.\nTéma: [thread]\n\nBejegyzés:\n[post]");
+define("LAN_FORUM_NT_NEWTOPIC_PROB_MSG", "[user] új felhasználó egy új témát hozott létre a(z) [forum] fórumban.\nTéma: [thread]\n\nBejegyzés:\n[post]");
+define("LAN_FORUM_NT_TOPIC_UPDATED_MSG", "[user] a(z) [thread] témát frissítette a(z) [forum] fórumban");
+define("LAN_FORUM_NT_TOPIC_DELETED_MSG", "[user] a(z) [thread] témát törölte a(z) [forum] fórumban");
+define("LAN_FORUM_NT_TOPIC_MOVED_MSG", "[user] a(z) [thread] témát áthelyezte a(z) [forum] fórumból a(z) [forum2] fórumba");
+
+define("LAN_FORUM_NT_POST_CREATED_MSG", "[user] egy új bejegyzést tett közzé a(z) [forum] fórum [thread] témában.\nBejegyzés:\n[post]");
+define("LAN_FORUM_NT_POST_UPDATED_MSG", "[user] frissítette a bejegyzést a(z) [forum] fórum [thread] témában.\nBejegyzés:\n[post]");
+define("LAN_FORUM_NT_POST_DELETED_MSG", "[user] törölte a bejegyzést a(z) [forum] fórum [thread] témában #[postid].\n\nBejegyzés:\n[post]");
+
+?>

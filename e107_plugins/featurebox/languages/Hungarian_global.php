e176bfc76fb4f5327533d4a674a3139f8009e1fa
Gábor <yesszus@gmail.com>
2019-10-29 10:14:22 +0100

    Update Hungarian_global.php

--- e107_plugins/featurebox/languages/Hungarian_global.php
+++ e107_plugins/featurebox/languages/Hungarian_global.php
@@ -1,24 +1,18 @@
 <?php
 /*
-+--------------------------------------------------------------------------+
++---------------------------------------------------------------+
 |        e107 website content management system Hungarian Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2017/05/30 08:51:43
+|        Last Modified: 2019/10/29 9:44:32
 |
 |        $Author: Yesszus $
-|        Web: https://github.com/yesszus/e107-v2.x-Language-Packs/releases
+|        Web: https://github.com/e107translations/Hungarian/releases 
 |
-+--------------------------------------------------------------------------+
-*/
++---------------------------------------------------------------+
+*/ 
 
-define("LAN_PLUGIN_FEATUREBOX_NAME", "Feature Box");
-define("LAN_PLUGIN_FEATUREBOX_DESCRIPTION", "Hírek és egyéb elemek megjelenítése az oldal tetején egy animált területen.");
+define("LAN_PLUGIN_FEATUREBOX_NAME", "Szolgáltatásdoboz");
+define("LAN_PLUGIN_FEATUREBOX_DESCRIPTION", "A hírek és egyéb elemek megjelenítése az oldal tetején egy animált területen.");
 define("LAN_PLUGIN_FEATUREBOX_BATCH", "Elem hozzáadása");
-
-define("FBLAN_INSTALL_01", "Alapértelmezett kategóriák hozzáadása.");
-define("FBLAN_INSTALL_02", "Alapértelmezett értékek hozzáadása.");
-define("FBLAN_INSTALL_03", "Nincs hozzárendelve");
-define("FBLAN_INSTALL_04", "Carousel");
-define("FBLAN_INSTALL_05", "Fülek");
-?>
+define("LAN_PLUGIN_FEATUREBOX_RSSFEED", "Ez a szolgáltatásdoboz bejegyzéseinek rss hírcsatornája.");

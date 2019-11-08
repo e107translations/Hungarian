16bd71edd282f3d0befc5ecffa0b218c3b28e416
Gábor <yesszus@gmail.com>
2019-11-08 06:29:30 +0100

    Update Hungarian_global.php

--- e107_plugins/download/languages/Hungarian/Hungarian_global.php
+++ e107_plugins/download/languages/Hungarian/Hungarian_global.php
@@ -1,8 +1,22 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
-
+/*
++--------------------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2019/11/11 06:30:33
+|
+|        $Author: Yesszus $
+|        Web: https://github.com/e107translations/Hungarian/releases
+|
++--------------------------------------------------------------------------+
+*/
 define("LAN_PLUGIN_DOWNLOAD_NAME", "Letöltések");
 define("LAN_PLUGIN_DOWNLOAD_DIZ", "Ez a plugin egy teljes értékű letöltés-kezelő rendszer.");
+
+define("LAN_DL_NT_01", "A hibás letöltés bejelentve");
+define("LAN_DL_NT_02", "A következő letöltés hibás letöltésként lett bejelentve:"); 
+define("LAN_DL_NT_03", "[x] bejelentés megjegyzése:");
+define("LAN_DL_NT_04", "Klikk [ide] a hibás letöltés bejelentés megtekintéséhez.");
+
+define("LAN_DL_LATEST_01", "Bejelentett letöltések");

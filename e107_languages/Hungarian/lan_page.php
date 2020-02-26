a8b9a4c9ae9d8a0615f5fa9566d486f733a53dc7
Gábor <yesszus@gmail.com>
2020-02-26 16:10:33 +0100

    Update lan_page.php

--- e107_languages/Hungarian/lan_page.php
+++ e107_languages/Hungarian/lan_page.php
@@ -4,10 +4,10 @@
 |        e107 website content management system Hungarian Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2017/01/15 11:15:43
+|        Last Modified: 2020/02/26 16:10:55
 |
 |        $Author: Yesszus $
-|        Web: https://github.com/yesszus/e107-v2.x-Language-Packs/releases
+|        Web: https://github.com/e107translations/Hungarian/releases
 |
 +--------------------------------------------------------------------------+
 */
@@ -27,4 +27,5 @@ define("LAN_PAGE_13", "Oldal");
 define("LAN_PAGE_14", "<!--Egyéb Oldalak-->");
 define("LAN_PAGE_15", "Oldalak");
 define("LAN_PAGE_16", "Ebben a könyvben jelenleg nem található egyetlen fejezet sem");
+define("LAN_PAGE_17", "A hozzászólás kikapcsolva!")
 ?>

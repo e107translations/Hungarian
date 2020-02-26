9683f0adab69177dcba83320c7049726c86d6e72
Gábor <yesszus@gmail.com>
2020-02-26 16:34:48 +0100

    Update lan_userclass2.php

--- e107_languages/Hungarian/admin/lan_userclass2.php
+++ e107_languages/Hungarian/admin/lan_userclass2.php
@@ -4,22 +4,22 @@
 |        e107 website content management system Hungarian Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2017/01/21 12:23:21
+|        Last Modified: 2020/02/26 16:31:33
 |
 |        $Author: Yesszus $
-|        Web: https://github.com/yesszus/e107-v2.x-Language-Packs/releases
+|        Web: https://github.com/e107translations/Hungarian/releases
 |
 +--------------------------------------------------------------------------+
 */
-
+/*
 define("UCSLAN_3", "Csoport törölve.");
 define("UCSLAN_5", "Csoport frissítve.");
 define("UCSLAN_6", "Csoport elmentve az adatbázisban.");
 define("UCSLAN_7", "Nincsenek csoportok.");
 define("UCSLAN_8", "Létező csoportok");
-define("UCSLAN_9", "Nincs Ikon");
+define("UCSLAN_9", "Nincs Ikon");*/
 define("UCSLAN_10", "Csoport törlése nem lehetséges - ez egy core csoport, vagy a definícióját használja egy másik csoport, mint szülő, vagy szerkeszd át a csoportot");
-define("UCSLAN_11", "Megerősítés");
+/*define("UCSLAN_11", "Megerősítés");
 define("UCSLAN_12", "Csoport név");
 define("UCSLAN_13", "Csoport ismertetés");
 define("UCSLAN_14", "Csoport frissítése");
@@ -31,22 +31,22 @@ define("UCSLAN_24", "Kik módosíthatják a csoportot");
 define("UCSLAN_25", "Csoport létrehozása/módosítása");
 define("UCSLAN_26", "Csoport tagság");
 define("UCSLAN_27", "Hibakeresés Súgó");
-define("UCSLAN_29", "Ez a csoport nem törölhető");
+define("UCSLAN_29", "Ez a csoport nem törölhető");*/
 define("UCSLAN_30", "Rövid név jelenik meg a választóban");
 define("UCSLAN_31", "Információ a csoport alkalmazhatóságáról");
 define("UCSLAN_32", "Ennek a csoportnak a tagjai hozzáadhatják/törölhetik magukat a módosítás alatt lévő csoporthoz/csoportból");
 define("UCSLAN_33", "Meghatározza, kik láthatják ezt a csoportot egy legördülő listában");
-define("UCSLAN_34", "Csoportot láthatja");
-define("UCSLAN_35", "Szülő csoport");
+//define("UCSLAN_34", "Csoportot láthatja");
+//define("UCSLAN_35", "Szülő csoport");
 define("UCSLAN_36", "Ha a fa tetején a 'Senki' van, akkor a jogosultságok tovább növekednek a fa teteje felé<br />Ha a fa tetején 'Mindenki' van, akkor a jogosultságok növekednek, ahogy csökken a fa");
 define("UCSLAN_37", "Írj be egy csoport nevet");
 define("UCSLAN_38", "Kezdő csoport");
 define("UCSLAN_39", "Nincs kiválasztható csoport");
 define("UCSLAN_40", "Válaszd ki a kezdő csoportokat");
 define("UCSLAN_41", "Beállítások frissítve");
-define("UCSLAN_42", "Nincs változtatás - nincs frissítés");
+//define("UCSLAN_42", "Nincs változtatás - nincs frissítés");
 define("UCSLAN_43", "Létező csoportok: ");
-define("UCSLAN_44", "Nincs");
+//define("UCSLAN_44", "Nincs");
 define("UCSLAN_45", "Pont a kiválasztott csoportoknál:");
 define("UCSLAN_46", "(kihagyás, ha nincs ellenőrzés)");
 define("UCSLAN_47", "Kezdő regisztráció");
@@ -61,16 +61,16 @@ define("UCSLAN_55", "Felhasználói hierarchia ürítése");
 define("UCSLAN_56", "(ezek a beállítások egy  'egyszerű' csoport struktúra)");
 define("UCSLAN_57", "(a hierarchiát később is módosíthatod)");
 define("UCSLAN_58", "Végrehajtás");
-define("UCSLAN_59", "Csoport módosítások admin naplózásának engedélyezése");
-define("UCSLAN_60", "Csoport konfigurációs lehetőségek");
-define("UCSLAN_61", "Csoport beállítása");
+//define("UCSLAN_59", "Csoport módosítások admin naplózásának engedélyezése");
+//define("UCSLAN_60", "Csoport konfigurációs lehetőségek");
+//define("UCSLAN_61", "Csoport beállítása");
 define("UCSLAN_62", "Alapértelmezett csoport fa létrehozása: ");
 define("UCSLAN_63", "A csoportnév már létezik - válassz másikat");
 define("UCSLAN_64", "kész");
 define("UCSLAN_65", "Egyszerű csoport hierarchia: ");
-define("UCSLAN_66", "Egyszerő csoport hierarchia megerősítés");
-define("UCSLAN_67", "Alapértelmezett csoport hierarchia beállításának megerősítése");
-define("UCSLAN_68", "Csoport Ikon");
+//define("UCSLAN_66", "Egyszerő csoport hierarchia megerősítés");
+//define("UCSLAN_67", "Alapértelmezett csoport hierarchia beállításának megerősítése");
+//define("UCSLAN_68", "Csoport Ikon");
 define("UCSLAN_69", "Csoporttal opcionálisan társított ikon - könyvtár ");
 define("UCSLAN_70", "Csoport hierarchia újraépítése: ");
 define("UCSLAN_71", "Csoport karbantartása");
@@ -81,12 +81,12 @@ define("UCSLAN_75", "Regisztrált és bejelentkezett tagok");
 define("UCSLAN_76", "Adminisztrátorok");
 define("UCSLAN_77", "Főadminisztrátorok");
 define("UCSLAN_78", "Fórum és egyéb területek moderátorai");
-define("UCSLAN_79", "Csoport típusa");
+//define("UCSLAN_79", "Csoport típusa");
 define("UCSLAN_80", "Normál");
 define("UCSLAN_81", "Összevont");
 define("UCSLAN_82", "Az összevont egyesíti az egyedi csoportokat");
 define("UCSLAN_83", "Összevont csoportok");
-define("UCSLAN_84", " (Összevont)");
+//define("UCSLAN_84", " (Összevont)");
 define("UCSLAN_85", "Megadtad az összes elérhető csoportot, töröld azt, amelyikre nincs szükség");
 define("UCSLAN_86", "Néhány beállítás nincs engedélyezve az admin csoportoknak - azért, mert ezek alapértelmezettek. ");
 define("UCSLAN_87", "Nemrég csatlakozott felhasználók");
@@ -95,5 +95,4 @@ define("UCSLAN_89", "A kijelőlt összevont csoportok");
 define("UCSLAN_90", "A rendszer bizonyos csoportjait nem tudod módosítani!");
 define("UCSLAN_91", "Csoport felépítés");
 
-define("UCSLAN_UPDATE", "Frissítés");
-?>
+//define("UCSLAN_UPDATE", "Frissítés");

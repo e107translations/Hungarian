8d687a518fcd505889dda8482f1357821a80b3dd
Gábor <yesszus@gmail.com>
2020-02-28 07:28:29 +0100

    Update Hungarian_global.php

--- e107_plugins/social/languages/Hungarian/Hungarian_global.php
+++ e107_plugins/social/languages/Hungarian/Hungarian_global.php
@@ -4,14 +4,16 @@
 |        e107 website content management system Hungarian Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2016/05/22 14:04:14
+|        Last Modified: 2020/02/28 07:12:33
 |
 |        $Author: Yesszus $
+|        Web: https://github.com/e107translations/Hungarian/releases
+|
 +---------------------------------------------------------------+
 */
+define("LAN_SOCIAL_ADMIN_SUMM", "Facebook, Twitter és egyéb közösségi média kiegészítők hozzáadása az e107 rendszerhez.");
 define("LAN_PLUGIN_SOCIAL_DESCR",  "Hozzáadja és felcseréli az e107 hozzászólás rendszert a Facebook rendszerre. Twitter hírek hozzáadása a weboldalra. stb.");
 define("LAN_PLUGIN_SOCIAL_SIGNIN",  "Regisztráció ezzel:");
 define("LAN_PLUGIN_SOCIAL_XUP_SIGNUP", "Regisztráció a [x] fiókkal"); 
 define("LAN_PLUGIN_SOCIAL_XUP_REG", "Regisztráció a [x] fiókkal");
 define("LAN_PLUGIN_SOCIAL_NAME", "Közösség");
-?>

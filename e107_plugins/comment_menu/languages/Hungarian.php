f6225d42e1b035a9a37e977f187bac19aa7800a7
Gábor <yesszus@gmail.com>
2020-10-01 11:54:01 +0200

    Update Hungarian.php

--- e107_plugins/comment_menu/languages/Hungarian.php
+++ e107_plugins/comment_menu/languages/Hungarian.php
@@ -1,8 +1,16 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
+/*
++--------------------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2020/05/04 10:16:28
+|
+|        $Author: Yesszus $
+|        Web: https://github.com/e107translations/Hungarian/releases
+|
++--------------------------------------------------------------------------+
+*/
 
 define("CM_L1", "Még nem érkezett hozzászólás.");
 define("CM_L2", "");
@@ -14,4 +22,4 @@ define("CM_L7", "Hír címének használata a menüben");
 define("CM_L8", "Hozzászólások menü beállítása");
 define("CM_L11", "ekkor");
 define("CM_L12", "Válasz:");
-define("CM_L13", "Írta:");
+define("CM_L13", "Beküldő:");

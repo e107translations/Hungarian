8d7a2ebfb432b3c8528606eb0175dd12a99b05b1
Gábor <yesszus@gmail.com>
2021-02-27 11:10:55 +0100

    Create Hungarian_global.php

---
+++ e107_plugins/hero/languages/Hungarian/Hungarian_global.php
@@ -0,0 +1,15 @@
+<?php
+/*
++----------------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2021/02/27 11:10:23
+|
+|        $Author: Yesszus $
++----------------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_HERO_NAME", "Hero");
+define("LAN_PLUGIN_HERO_SUMMARY", "Kezelje a kezdőlapját a 'Hero' bővítménnyel");
+define("LAN_PLUGIN_HERO_DESCRIPTION", "Kép és szöveg slider, animált pontokkal a hero területtel az ön kezdőlapjára.");

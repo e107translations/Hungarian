2250e92f00bfeac90d8d0b50ab26b6b552dab9d9
Gábor <yesszus@gmail.com>
2021-02-27 11:20:52 +0100

    Update lan_meta.php

--- e107_languages/Hungarian/admin/lan_meta.php
+++ e107_languages/Hungarian/admin/lan_meta.php
@@ -1,20 +1,21 @@
 <?php
 /*
-+---------------------------------------------------------------+
++----------------------------------------------------------------------+
 |        e107 website content management system Hungarian Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2016/05/17 21:28:48
+|        Last Modified: 2021/02/27 10:28:23
 |
 |        $Author: Yesszus $
-|        Web: https://github.com/yesszus/e107-v2.x-Language-Packs/releases
-|
-+---------------------------------------------------------------+
++----------------------------------------------------------------------+
 */
 
 define("METLAN_00", "Meta Tag-ek");
 
 define("METLAN_1", "További meta Tag-ek");
 define("METLAN_2", "Pl.: < meta name='revisit-after' content='30 days' />");
-define("METLAN_3", "Használja a Hírek címét és az összegzést, meta ismertetésként a Hírek oldalon.");
-?>
+define("METLAN_3", "A hírek címének és összegzésének használata meta ismertetésként a Hírek oldalon.");
+define("METLAN_4", "Egyéni címkék a ([x] címkéken belül)");
+define("METLAN_5", "Egyéni címkék a ([x] után)");
+define("METLAN_6", "Egyéni címkék a ([x] előtt)");
+define("METLAN_7", "Minden ide beírt metaadat vagy egyéni HTML-címke (például <script> címkék vagy Google elemzési kód) a weboldal minden oldalán megjelennek a kijelölt területeken. ");

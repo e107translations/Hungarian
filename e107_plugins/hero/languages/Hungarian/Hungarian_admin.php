4d430673e5da1642f9bf6aa0d43afe9ec52f98f8
Gábor <yesszus@gmail.com>
2021-02-27 11:11:56 +0100

    Create Hungarian_admin.php

---
+++ e107_plugins/hero/languages/Hungarian/Hungarian_admin.php
@@ -0,0 +1,26 @@
+<?php
+/*
++----------------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2021/02/27 11:10:23
+|
+|        $Author: Yesszus $
++----------------------------------------------------------------------+
+*/
+define("LAN_HERO_ADMIN_001", "Az alapértelmezett slide importálva lett!");
+define("LAN_HERO_ADMIN_002", "Az alapértelmezett slide nem lett importálva!");
+define("LAN_HERO_ADMIN_003", "Háttér");
+define("LAN_HERO_ADMIN_004", "Kép/Videó");
+define("LAN_HERO_ADMIN_005", "Média");
+define("LAN_HERO_ADMIN_006", "Címke");
+define("LAN_HERO_ADMIN_007", "Ügyeljen arra, hogy a [Menükezelő] segítségével engedélyezze a hero menüt a kezdőlapon.");
+define("LAN_HERO_ADMIN_009", "Ikonok");
+define("LAN_HERO_ADMIN_010", "Ikon-stilus");
+define("LAN_HERO_ADMIN_011", "Váltási időköz");
+define("LAN_HERO_ADMIN_012", "Szöveg");
+define("LAN_HERO_ADMIN_013", "Animáció");
+define("LAN_HERO_ADMIN_014", "Késleltetés (mp)");
+define("LAN_HERO_ADMIN_015", "Gomb");
+define("LAN_HERO_ADMIN_016", "[x] másodperc");

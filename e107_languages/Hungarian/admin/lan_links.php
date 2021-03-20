f17dc276e027d0a3e484d47c8e5c66eed475aadf
Erdei Gábor <yesszus@gmail.com>
2021-03-20 15:35:25 +0100

    Update lan_links.php

--- e107_languages/Hungarian/admin/lan_links.php
+++ e107_languages/Hungarian/admin/lan_links.php
@@ -1,16 +1,17 @@
 <?php
 /*
-+---------------------------------------------------------------+
++--------------------------------------------------------------------------+
 |        e107 website content management system Hungarian Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2016/02/22 13:08:21
+|        Last Modified: 2021/03/17 02:17:32
 |
 |        $Author: Yesszus $
-|        Web: https://github.com/yesszus/e107-v2.x-Language-Packs/releases
+|        Web: https://github.com/e107translations/Hungarian/releases
 |
-+---------------------------------------------------------------+
++--------------------------------------------------------------------------+
 */
+
 define("LCLAN_15", "Link neve");
 define("LCLAN_19", "Megnyitás módja");
 define("LCLAN_20", "Azonos ablakban");
@@ -29,6 +30,11 @@ define("LCLAN_108", "Néhány beállítás kihagyva - nem tudod kiválasztani a 
 define("LCLAN_109", "Válassz egy kezdőt (szülőt)");
 define("LCLAN_110", "Válassz ki egy generátor modult");
 define("LCLAN_111", "Nincs érvényes generátor modul adat");
+define("LCLAN_112", "1 - Fő");
+define("LCLAN_113", "2 - Oldalsáv");
+define("LCLAN_114", "3 - Lábléc");
+define("LCLAN_115", "Alt");
+define("LCLAN_116", "(Kiosztatlan)");
 
 define("LINKLAN_1", "Megnyitás 800x600-as ablakban");
 define("LINKLAN_4", "Al-link Generátor");
@@ -38,4 +44,3 @@ define("LINKLAN_7", "Melyik link alá készítsen al-linket?");
 define("LINKLAN_8", "Hír kategóriák");
 define("LINKLAN_9", "Letöltés kategóriák");
 define("LINKLAN_10", "Sablon Rövidkódok");
-?>

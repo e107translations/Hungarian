b244cb226acb0d27526e7d86bee81d83560b7987
Erdei Gábor <yesszus@gmail.com>
2021-04-02 08:24:32 +0200

    Update lan_contact.php

--- e107_languages/Hungarian/lan_contact.php
+++ e107_languages/Hungarian/lan_contact.php
@@ -4,7 +4,7 @@
 |        e107 website content management system Hungarian Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2018/09/13 10:13:43
+|        Last Modified: 2021/04/02 08:22:34
 |
 |        $Author: Yesszus $
 |        Web: https://github.com/e107translations/Hungarian/releases
@@ -12,29 +12,28 @@
 +--------------------------------------------------------------------------+
 */
 
-define("LANCONTACT_00", "Lépj kapcsolatba velünk");
-define("LANCONTACT_01", "Kapcsolat részletei");
-define("LANCONTACT_02", "Kapcsolatfelvételi űrlap");
-define("LANCONTACT_03", "Kérlek, add meg a neved:");
-define("LANCONTACT_04", "Kérlek, add meg az email címed:");
-define("LANCONTACT_05", "Üzeneted tárgya:");
-define("LANCONTACT_06", "Üzeneted:");
-define("LANCONTACT_07", "Szeretnék kapni egy másolatot az email címemre");
-define("LANCONTACT_08", "Küldés");
-define("LANCONTACT_09", "Az üzeneted el lett küldve.");
-define("LANCONTACT_10", "Hiba történt az üzeneted elküldése közben.");
-define("LANCONTACT_11", "Az általad megadott email cím érvénytelen.\\nKérlek ellenőrizd, majd próbáld meg újra.");
-define("LANCONTACT_12", "Az általad megadott üzenet túl rövid.");
-define("LANCONTACT_13", "Kérlek, adj meg egy témát az üzenet elküldéséhez.");
-define("LANCONTACT_14", "Üzenet küldése neki(k):");
-define("LANCONTACT_15", "Hibás kódot adtál meg");
-define("LANCONTACT_16", "Az üzeneted elküldéséhez [registered] és jelentkezz be.");
-define("LANCONTACT_17", "Adja meg a nevét!");
-define("LANCONTACT_18", "Adja meg az e-mail címét!");
-define("LANCONTACT_19", "Adja meg az e-mail tárgyát!");
-define("LANCONTACT_20", "Adja meg az üzenet szövegét!");
-define("LANCONTACT_21", "Hozzájárulok ahhoz, hogy ez a weboldal tárolja az információkat, hogy válaszolhassanak a kérdésemre.");
-define("LANCONTACT_22", "Adatvédelmi irányelvek");
-define("LANCONTACT_23", "Itt találja meg az adatvédelmi irányelveinket: [x]");
-define("LANCONTACT_24", "GDPR megállapodás");
-?>
+define("LAN_CONTACT_00", "Lépj kapcsolatba velünk");
+define("LAN_CONTACT_01", "Kapcsolat részletei");
+define("LAN_CONTACT_02", "Kapcsolatfelvételi űrlap");
+define("LAN_CONTACT_03", "Kérlek, add meg a neved:");
+define("LAN_CONTACT_04", "Kérlek, add meg az email címed:");
+define("LAN_CONTACT_05", "Üzeneted tárgya:");
+define("LAN_CONTACT_06", "Üzeneted:");
+define("LAN_CONTACT_07", "Szeretnék kapni egy másolatot az email címemre");
+define("LAN_CONTACT_08", "Küldés");
+define("LAN_CONTACT_09", "Az üzenet elküldve!");
+define("LAN_CONTACT_10", "Hiba történt az üzenet küldése közben!");
+define("LAN_CONTACT_11", "A megadott email cím érvénytelen, ellenőrizd, majd próbáld meg újra.");
+define("LAN_CONTACT_12", "Az általad megadott üzenet túl rövid.");
+define("LAN_CONTACT_13", "Kérlek, add meg az üzenet tárgyát.");
+define("LAN_CONTACT_14", "Üzenet küldése neki(k):");
+define("LAN_CONTACT_15", "Hibás kódot adtál meg");
+define("LAN_CONTACT_16", "Az üzeneted elküldéséhez [registered] és jelentkezz be.");
+define("LAN_CONTACT_17", "Adja meg a nevét!");
+define("LAN_CONTACT_18", "Adja meg az e-mail címét!");
+define("LAN_CONTACT_19", "Adja meg az e-mail tárgyát!");
+define("LAN_CONTACT_20", "Adja meg az üzenet szövegét!");
+define("LAN_CONTACT_21", "Hozzájárulok ahhoz, hogy a weboldal tárolja az információkat és válaszolhassanak a kérdésemre.");
+define("LAN_CONTACT_22", "Adatvédelmi irányelvek");
+define("LAN_CONTACT_23", "Itt találja meg az adatvédelmi irányelveinket: [x]");
+define("LAN_CONTACT_24", "GDPR megállapodás");

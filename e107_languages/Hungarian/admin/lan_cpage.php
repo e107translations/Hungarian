778400044c3e81e1dcae2a8ecfe1a1c0c33ddf3e
Erdei Gábor <yesszus@gmail.com>
2024-08-07 01:52:59 +0200

    Update lan_cpage.php

--- e107_languages/Hungarian/admin/lan_cpage.php
+++ e107_languages/Hungarian/admin/lan_cpage.php
@@ -4,10 +4,9 @@
 |        e107 website content management system Hungarian Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2016/04/09 11:33:59
+|        Last Modified: 2024/08/07
 |
 |        $Author: Yesszus $
-|        Web: https://github.com/yesszus/e107-v2.x-Language-Packs/releases
 |
 +-------------------------------------------------------------------------+
 */
